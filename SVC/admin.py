854983bafd0a7631e4335226d3cf1e670b8cc1ca
Hantsi Taumang <63910830+hanSiey@users.noreply.github.com>
2023-03-12 08:47:20 +0200

    new admin

--- SVC/admin.py
+++ SVC/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Student, Project 
+from .models import Student, Project, Staff, Occurrence, Participant
 
 class StudentAdmin(admin.ModelAdmin):
   list_display = ('name', 'surname', 'access_number', 'hours_completed')
 
 class ProjectsAdmin(admin.ModelAdmin):
-  list_display =('name', 'goal', 'date', 'project_type', 'venue', 'expected_duration')
+  list_display =('name', 'goal', 'project_type')
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Project, ProjectsAdmin)
+admin.site.register(Staff, StudentAdmin)
+admin.site.register(Occurrence)
+admin.site.register(Participant)
